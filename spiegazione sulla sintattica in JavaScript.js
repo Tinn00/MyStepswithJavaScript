5b0116894fd491dda4367a5ea6c7f706ed4cb8f2
Tinn <juanmartindiantoni@gmail.com>
2026-05-16 18:06:35 +0200

    Add files via upload

---
+++ spiegazione sulla sintattica in JavaScript.js
@@ -0,0 +1,20 @@
+//spiegazione sulla sintassi:
+let nome = `Juan Martin`;
+let eta = 25;
+let messaggio = `Ciao, mi chiamo ${nome} e ho ${eta} anni.`;
+console.log(messaggio);
+
+//spiegazione toUpperCase & replace: 
+let frase = `Espira e inspira per ${30} secondi.`;
+console.log(frase.toUpperCase());
+console.log(frase.replace(`inspira`, `trattieni`));
+
+//esercizio 3 di sintassi, quest'ultimo l'ho fatto per conto mio:
+let listadellaspesa = `arancie x ${6}, uova x ${5}, latte x ${2}, farina x ${3}.`;
+console.log(`queste sono le cose da comprare: ` + listadellaspesa);
+
+
+let numero1 = 10;
+let numero2 = 5.5;
+let somma = (numero1 + numero2);
+console.log(`il resto è: ` + somma);
