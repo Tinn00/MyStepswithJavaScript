c8907f66fdc759befa7f042d05889b76a66e362e
Tinn <juanmartindiantoni@gmail.com>
2026-05-16 18:03:19 +0200

    Add files via upload

---
+++ ciclo while esercizio.js
@@ -0,0 +1,17 @@
+let n
+do{
+n = Number(prompt("Dammi il numero: "));
+
+
+
+if (n!=0){
+    
+    
+    if (n%2==0){
+         console.log("è pari");
+         }
+    else{
+        console.log("è dispari");
+    }
+}
+}while (n!=0);
